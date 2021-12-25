7f3923d0d7acb9db54d090a18f696835ed50d16e
mehedi hasan khairul <mhkhairul78@gmail.com>
2021-12-25 12:51:01 +0600

    path find in web pages

--- findData.js
+++ findData.js
@@ -1,3 +1,20 @@
+// Writing Jquery
+
+// code for getting input from user
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const pathResult = document.getElementById("path");
+button.addEventListener("click", () => {
+  const inputValue = input.value;
+  console.log(inputValue);
+  const result = findObjectPath(objectTree, inputValue);
+  pathResult.innerHTML = result;
+});
+
+
+
+
+
 
 const objectTree = {
   a: {
@@ -41,20 +58,20 @@ const objectTree = {
   },
 }
 
- const findObjectPath = (tree, target) => {
-//  run for loop with Object.keys that will return an array of keys
-    for (const key of Object.keys(tree)) {
-      if (key === target) {
-        // if the key is equal to the target, return the key
-        return key;
-      } else if (key && typeof tree[key] === "object") {
-       
-        const myPath = findObjectPath(tree[key], target);
-        if (myPath) {
-          return `${key} => ${myPath}`;
-        }
+const findObjectPath = (tree, target) => {
+  //  run for loop with Object.keys that will return an array of keys
+  for (const key of Object.keys(tree)) {
+    if (key === target) {
+      // if the key is equal to the target, return the key
+      return key;
+    } else if (key && typeof tree[key] === "object") {
+
+      const myPath = findObjectPath(tree[key], target);
+      if (myPath) {
+        return `${key} => ${myPath}`;
       }
     }
-  };
+  }
+};
 
-console.log(findObjectPath(objectTree, "AA"));
+// console.log(findObjectPath(objectTree, "AA"));
